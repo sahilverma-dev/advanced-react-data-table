9eb71a93754f9d9843accddd33c1cbab2146d3e6
Sahil Verma <domainsahil007@gmail.com>
2025-12-18 17:30:21 +0530

    blank init

--- src/components/providers/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NuqsAdapter } from "nuqs/adapters/react";
-
-const Providers = ({ children }: React.PropsWithChildren) => {
-  return <NuqsAdapter>{children}</NuqsAdapter>;
-};
-
-export default Providers;
--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
+  server: { port: 3000 },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
