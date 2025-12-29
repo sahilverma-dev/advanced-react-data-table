d7a0ff4c14a4cdd1e595ee440ed24fdd9c6ba768
Sahil Verma <domainsahil007@gmail.com>
2025-12-29 12:51:42 +0530

    feat: add Sonner Toaster component to providers

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -1,7 +1,13 @@
 import { NuqsAdapter } from "nuqs/adapters/react";
+import { Toaster } from "sonner";
 
 const Providers = ({ children }: React.PropsWithChildren) => {
-  return <NuqsAdapter>{children}</NuqsAdapter>;
+  return (
+    <NuqsAdapter>
+      {children}
+      <Toaster richColors />
+    </NuqsAdapter>
+  );
 };
 
 export default Providers;
